dae1fd0a21e6b17313379b6057e691d5f08d8f65
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-03-14 14:02:08 +0530

    Add project files.

---
+++ ApiCRUDServer/EDM/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiCRUDServer.EDM
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Salary { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public string Department { get; set; }
+    }
+}
